17ad6776bed259e122bdbc96b414558cb3e1e8d0
Bibin Raphel <bibinraphela@gmail.com>
2025-06-08 15:38:14 +0530

    lean Upy
    Signed-off-by: Bibin Raphel <bibinraphela@gmail.com>

--- ledBlinking/build/led_c.d
+++
@@ -1,2 +0,0 @@
-build/led_c.o: src/led.c include/led.h include/peripherals/gpio.h \
- include/common.h include/peripherals/base.h include/utils.h
--- ledBlinking/build/main_c.d
+++
@@ -1,2 +0,0 @@
-build/main_c.o: src/main.c include/led.h include/peripherals/gpio.h \
- include/common.h include/peripherals/base.h include/utils.h
