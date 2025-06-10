553d169450412c6155f41966831583b7b817d2c8
Bibin Raphel <bibinraphela@gmail.com>
2025-06-10 19:46:43 +0530

    modified gic.c and irq.c
    Signed-off-by: Bibin Raphel <bibinraphela@gmail.com>

--- freeRTOS/gic.c
+++ freeRTOS/gic.c
@@ -1,3 +1,18 @@
-void gic_init(void) {
-    // Initialization code for GIC here
+#include "gic.h"
+
+
+void gic_init(void)
+{
+    // Enable Distributor
+    GICD_CTLR = 1;
+
+    // Enable CPU interface
+    GICC_CTLR = 1;
+
+    // Set priority mask to allow all priorities through (lowest = 0xFF)
+    GICC_PMR = 0xFF;
+
+    // Enable interrupt for IRQ ID 30 (example: timer interrupt)
+    GICD_ISENABLER0 = (1 << 30);
 }
+
--- freeRTOS/irq.c
+++ freeRTOS/irq.c
@@ -1,3 +1,37 @@
+#if 0
 void irq_c_handler(void) {
     // Your IRQ handling code here
+}
+#endif
+
+#include "gic.h"
+
+#define TIMER_IRQ 30  // Replace with your timer IRQ number
+#define UART_IRQ 33   // Replace with your UART IRQ number
+
+void timer_interrupt_handler(void) {
+    // Clear timer interrupt flag, reload timer, etc.
+}
+
+void uart_interrupt_handler(void) {
+    // Read UART status, clear interrupt, handle RX/TX
+}
+
+void irq_c_handler(void)
+{
+    uint32_t irq_id = gic_get_irq();
+
+    switch (irq_id) {
+        case TIMER_IRQ:
+            timer_interrupt_handler();
+            break;
+        case UART_IRQ:
+            uart_interrupt_handler();
+            break;
+        default:
+            // Unknown or spurious interrupt
+            break;
+    }
+
+    gic_acknowledge_irq(irq_id);
 }
